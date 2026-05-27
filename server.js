ff0e8f4c0dd60b8b6b37d5bd47b50ee336ed3aae
Serj <popp.sergiu@gmail.com>
2026-05-27 09:39:41 +0300

    changes to wevbsite

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
